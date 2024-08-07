3658353b7ed75b3159634df1324bc66d62eb2115
gemialb <gemisaraqini@gmail.com>
2024-08-07 12:16:53 +0200

    ajout d'index, update, fetch, update trailer fonctionnel

---
+++ includes/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+// Connexion à la base de données MySQL
+$host = 'localhost';
+$db = 'getflix_db';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Erreur de connexion : ' . $e->getMessage());
+}
+?>
