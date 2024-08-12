65f8a19d703a2eda24c68b5c00e193b53e2cddb9
gemialb <gemisaraqini@gmail.com>
2024-08-12 13:52:12 +0200

    add rel_date into bdd and movie.php

--- public/movie.php
+++ public/movie.php
@@ -1,90 +1,75 @@
 <?php
 require_once('../includes/db_connect.php');
 
-$idMovie = $_GET['id'];
+// Fonction pour échapper les données de sortie
+function escapeHtml($data) {
+    return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+}
 
-// URL de la base pour les images
-$base_image_url = 'https://image.tmdb.org/t/p/w500';
+// Récupération de l'identifiant du film depuis l'URL
+$idMovie = isset($_GET['id']) ? (int)$_GET['id'] : null;
 
-// URL de la base pour les images
+if (!$idMovie) {
+    die("L'identifiant du film est requis.");
+}
+
+// URL de base pour les images et les bandes-annonces
+$base_image_url = 'https://image.tmdb.org/t/p/w500/';
 $base_trailer = 'https://www.youtube.com/embed/';
 
-    // Prepare a secure SQL statement
-    $sql = "SELECT * FROM movies WHERE movies.id = :idMovie";
-    $stmt = $pdo->prepare($sql);
+// Préparation et exécution de la requête SQL
+$sql = "SELECT * FROM movies WHERE id = :idMovie";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':idMovie', $idMovie, PDO::PARAM_INT);
+$stmt->execute();
+$movie = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    // Bind parameters to prevent SQL injection
-    $stmt->bindParam(':idMovie', $idMovie, PDO::PARAM_STR);
+if (!$movie) {
+    die('Le film n\'existe pas.');
+}
 
-    // Execute the prepared statement
-    $stmt->execute();
-    $movie = $stmt->fetch(PDO::FETCH_ASSOC);
-    // VERIFIE SI MOVIES EST PAS VIDE
-    if(!empty($movie)){
-        // SI MOVIE PAS VIDE ON FAIT CE QUE ON A FAIRE
-  //      $movie = json_encode($movie, JSON_UNESCAPED_UNICODE);
-        var_dump($movie);
-    } else {
-        // PAS DE MOVIE SOIT ON AFFICHE MESSAGE INEXISTANT SOIT REDIRECTION VERS INDEX
-        echo 'existe pas';
-    }
+// Extraction des données du film et échappement
+$title = escapeHtml($movie['title']);
+$overview = escapeHtml($movie['overview']);
+$movie_image = escapeHtml($base_image_url . $movie['movies_image']);
+$trailer_url = escapeHtml($base_trailer . $movie['movies_trailer']);
+$rating = escapeHtml($movie['rating']);
+$artist_name = escapeHtml($movie['artist_name']);
+$artist_image_url = $movie['artist_image_url'];
+$release_date = escapeHtml($movie['release_date']); // Ajout de la date de sortie
+
 ?>
 
-<h1>
-    <?php 
-    echo $movie['title'];?>
-</h1>
+<h1><?php echo $title; ?></h1>
 
 <div class="overview">
-<?php 
-echo $movie['overview'];
-?>
+    <?php echo $overview; ?>
 </div>
 
 <div class="movieImage">
-    <img src="<?php echo htmlspecialchars($base_image_url . ($movie['movies_image']));?>"
+    <img src="<?php echo $movie_image; ?>" alt="Image du film">
 </div>
 
 <div>
-    <iframe class="trailer" width="420" height="315" src="<?php echo ($base_trailer . ($movie['movies_trailer']));?>"></iframe>
-    <style> 
+    <iframe class="trailer" width="420" height="315" src="<?php echo $trailer_url; ?>" frameborder="0" allowfullscreen></iframe>
+    <style>
         .trailer {
-            position: none;
-            
+            position: relative;
         }
     </style>
 </div>
 
 <div class="rating">
-    <?php echo $movie['rating'] . '/10'; ?>
+    <?php echo $rating . '/10'; ?>
 </div>
 
 <div class="actor_name">
-    <?php echo $movie['artist_name']; ?>
+    <?php echo $artist_name; ?>
 </div>
 
 <div class="actor_image">
-<?php
-// Base URL pour les images
-$base_image_url = "https://image.tmdb.org/t/p/w500/";
-
-// Récupération de l'identifiant du film depuis l'URL ou autre source
-if (isset($_GET['id'])) {
-    $movie_id = (int)$_GET['id']; // Convertit l'id en entier pour plus de sécurité
-} else {
-    die("L'identifiant du film est requis.");
-}
-
-// Préparer et exécuter la requête pour récupérer la colonne 'artist_image_url'
-try {
-    $stmt = $pdo->prepare("SELECT artist_image_url FROM movies WHERE id = :id");
-    $stmt->execute(['id' => $movie_id]);
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    // Vérifiez si la colonne 'artist_image_url' est récupérée
-    if ($row && isset($row['artist_image_url'])) {
-        $artist_image_url = $row['artist_image_url'];
-
+    <?php
+    if ($artist_image_url) {
         // Séparer les chemins d'image en un tableau
         $image_paths = explode(', ', $artist_image_url);
 
@@ -95,13 +80,12 @@ try {
 
         // Affiche les URLs des images
         foreach ($image_urls as $url) {
-            echo "<img src=\"$url\" alt=\"Artist Image\"><br>";
+            echo "<img src=\"$url\" alt=\"Image de l'artiste\"><br>";
         }
     } else {
         echo "Aucune image trouvée.";
     }
-} catch (PDOException $e) {
-    die("Query failed: " . $e->getMessage());
-}
-?>
+    ?>
 </div>
+
+<p>Date de sortie : <?php echo $release_date; ?></p>
