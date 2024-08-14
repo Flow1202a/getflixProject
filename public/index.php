c7d52eb3fbc6efeeb3b7842510a9f1e62f4749bc
Flow1202a <aliuflorian8@gmail.com>
2024-08-14 15:18:10 +0200

    save safe

--- public/index.php
+++ public/index.php
@@ -18,7 +18,8 @@ $movies = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../style/style.css">
     <script src="../javascript/script.js" defer></script>
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
+
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Welcome</title>
 </head>
