a71d83e127becabd736833598218b0dad07122dc
Flow1202a <aliuflorian8@gmail.com>
2024-09-06 16:00:21 +0200

    test

--- public/search_results.php
+++ public/search_results.php
@@ -29,22 +29,35 @@ if ($search_term) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+<<<<<<< HEAD
 
 <header>
     <nav class="nav position-fixed w-100">
         <i class="uil uil-bars navOpenBtn"></i>
         <a href="index.php" class="logo">GetFlixDeNullos</a>
+=======
+    <header>
+    <nav class="nav position-fixed w-100">
+        <i class="uil uil-bars navOpenBtn"></i>
+            <a href="index.php" class="logo">GetFlixDeNullos</a>
+>>>>>>> 93c2816 (modif)
 
         <ul class="nav-links align-items-center">
             <i class="uil uil-times navCloseBtn"></i>
             <li><a href="index.php">Home</a></li>
             <li><a href="categories.php">Categories</a></li>
+<<<<<<< HEAD
             <li><a href="../public/account.php">Account</a></li>
             <?php if (isset($_SESSION['role'])): ?>
                 <li><a href="../includes/logout.php">Se déconnecter</a></li>
             <?php else: ?>
                 <li><a href="logintest.php">Connexion</a></li>
             <?php endif; ?>
+=======
+            <li><a href="#">WatchList</a></li>
+            <li><a href="../includes/back_office.php">Account</a></li>
+            <li><a href="logintest.php">Connexion</a></li>
+>>>>>>> 93c2816 (modif)
         </ul>
 
         <i class="uil uil-search search-icon" id="searchIcon"></i>
@@ -58,6 +71,7 @@ if ($search_term) {
             </form>
         </div>
     </nav>
+<<<<<<< HEAD
 </header>
 
 <div class="search-results-container" style="padding-top: 80px;">
@@ -73,5 +87,24 @@ if ($search_term) {
     <?php endif; ?>
 </div>
 <script src="../javascript/script.js" defer></script>
+=======
+
+    </header>
+    <div class="results-container">
+        <?php if (empty($movies)): ?>
+            <p>Aucun résultat trouvé.</p>
+        <?php else: ?>
+            <?php foreach ($movies as $movie): ?>
+                <div class="card moviecarte col-xs-12 col-sm-6 col-md-3 col-lg-2 mb-5 me-1 ">
+                    <img src="<?php echo htmlspecialchars($base_image_url . htmlspecialchars($movie['movies_image'])); ?>" class="card-img-top" alt="<?php echo htmlspecialchars($movie['title']); ?>">
+                    <div class="card-body align-content-center">
+                        <h5 class="card-title" style="color: #EED6D3"><?php echo htmlspecialchars($movie['title']); ?></h5>
+                        <a  href="movie.php?id=<?php echo htmlspecialchars($movie['id']); ?>" class="btn boutonDetails btn-primary">Voir les détails</a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
+>>>>>>> 93c2816 (modif)
 </body>
 </html>
